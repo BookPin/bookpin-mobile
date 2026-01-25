9b4e7ec8ea6b20de25ac17ff879533c2b2035259
jeongjaino <akgk155@gmail.com>
2026-01-25 22:01:45 +0900

    #2 : designSystem 네이밍 수정

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -28,7 +28,7 @@ kotlin {
         iosSimulatorArm64(),
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "dessign-systemKit"
+            baseName = "DesignSystemKit"
         }
     }
 
