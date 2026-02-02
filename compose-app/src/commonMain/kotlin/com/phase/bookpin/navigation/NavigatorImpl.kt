24b6f981ea2208852f781cf89cc8f4a96cc81423
jeongjaino <akgk155@gmail.com>
2026-02-02 23:53:43 +0900

    navigator 인터페이스 제거

--- compose-app/src/commonMain/kotlin/com/phase/bookpin/navigation/NavigatorImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.phase.bookpin.navigation
-
-import com.phase.bookpin.data.api.navigation.Navigator
-
-class NavigatorImpl : Navigator {
-    override fun openAuth() {
-        // TODO: Implement navigation to auth screen
-    }
-}
--- data-api/src/commonMain/kotlin/com/phase/bookpin/data/api/navigation/Navigator.kt
+++
@@ -1,5 +0,0 @@
-package com.phase.bookpin.data.api.navigation
-
-interface Navigator {
-    fun openAuth()
-}
